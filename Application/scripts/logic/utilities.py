48b14fd7e0b7f47a3f203b05448dd12aa2a1c425
Ovsienko023 <Ovsienko023@gmail.com>
2020-08-26 15:17:20 +0300

    add ConnectDB and WrapperDB

--- Application/scripts/logic/utilities.py
+++ Application/scripts/logic/utilities.py
@@ -1,37 +1,44 @@
-import pymongo
 import os
 import json
-import sys
-import time
-from bson.objectid import ObjectId
-OS = sys.platform
+import psycopg2
+# from bson.objectid import ObjectId
+# OS = sys.platform
 
 
 class ConnectDB():
+    """Class for connect to DB."""
     def __init__(self):
-        self.info_db = self.get_info_db()
-        self.conn = pymongo.MongoClient(*self.info_db)
-        self.db = self.conn.mydb
+        self.conn = psycopg2.connect(**self.config_app()['Data_Base'])
+        self.cursor = self.conn.cursor()
 
     def config_app(self):
-        path = os.getcwd() + "/Application-VT/config.json"
+        path = os.getcwd() + "\Application\config.json" ######### !!!\config.json
         with open(path) as config:
             json_str = config.read()
             return json.loads(json_str)
 
-    def get_info_db(self):
-        info_db = self.config_app()['Data_Base']
-        host, port = info_db['host'], info_db['port']
-        return host, int(port)
-
+    def close(self):
+        self.conn.close()
+        self.cursor.close()
 
 
 class WrapperDB:
+    """Class for requests DB."""
+    #self.connect_db.conn.commit()
     def __init__(self):
-        self.db = ConnectDB().db
-        self.coll_users = self.db.users
-        self.coll_message = self.db.message
+        self.connect_db = ConnectDB()
+
+    def get_all_coordinate(self) -> list:
+        """  """
+        request = """ SELECT * FROM coordinate """
+        self.connect_db.cursor.execute(request)
+        return self.connect_db.cursor.fetchall()
     
+    def __str__(self):
+        return __class__.__name__
+    
+    
+
 
 class Destributor:
     def __init__(self):
--- Application/testing/create_db.py
+++ Application/testing/create_db.py
@@ -9,3 +9,27 @@ def creat_db():
     coll_users = db['users']
 
 creat_db()
+
+
+48.704578, 44.507112
+create_table_cargo = """CREATE TABLE coordinate(
+                            Id serial primary key,
+                            Lat DECIMAL(9,6),
+                            Lon DECIMAL(9,6)
+                    );"""
+
+create_INDEX = """CREATE INDEX index_name ON coordinate (Lat, Lon);
+                """
+
+insert_test_coor = """ INSERT INTO coordinate (Lat, Lon) VALUES(48.685444, 44.474254), 
+                                                     (48.716087, 44.482757), 
+                                                     (48.746087, 44.697221); """
+
+
+
+in_circle = """
+Select id, Lat, Lon,
+       acos(sin(radians(48.704578))*sin(radians(Lat)) + cos(radians(48.704578))*cos(radians(Lat))*cos(radians(Lon)-radians(44.507112))) * 6371 As D
+From coordinate
+Where acos(sin(radians(48.704578))*sin(radians(Lat)) + cos(radians(48.704578))*cos(radians(Lat))*cos(radians(Lon)-radians(44.507112))) * 6371 < 5;
+"""
